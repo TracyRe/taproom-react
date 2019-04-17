783e2f670827bed725eb9b56c863bc465c5316c7
Tracy Reith <treith@treith.com>
2019-04-16 21:10:15 -0700

    Setup project and build Help Queue sample site

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Ticket(props) {
+  return (
+    <div>
+      <style jsx>{`
+      div {
+        background-color: #f42;
+      }
+      `}</style>
+      <h3>{props.location} - {props.names}</h3>
+      <p>{props.issue}</p>
+      <hr/>
+    </div>
+  )
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,  location:PropTypes.string.isRequired,
+  issue: PropTypes.string,
+}
+export default Ticket
