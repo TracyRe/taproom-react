48ba8e2cd6163973fcb7a692f494267c4a26c598
Tracy Reith <treith@treith.com>
2019-04-19 14:07:22 -0700

    Added path to 404 page, confirmed 404 redirect works

--- src/Error404.jsx
+++ src/Error404.jsx
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function Error404() {
+function Error404(props) {
 
   const Error404 =  {
-    background: '#f00',
+    background: '#fff',
   }
 
 
   return (
     <div style={Error404}>
-      <h3>Page not found</h3>
+      <h3>Page {props.location.pathname} not found</h3>
       <p><Link to='/'>Back to the bar</Link></p>
     </div>
   )
 }
+
+Error404.propTypes = {
+  location: PropTypes.object
+};
 
 export default Error404
