78bcd7cab23492e71dd2fc692b384b0ba6e76494
Tracy Reith <treith@treith.com>
2019-04-26 11:47:44 -0700

    Attempt to make populated HTML form editable as a stateful component

--- src/components/KegChange.jsx
+++ src/components/KegChange.jsx
@@ -4,86 +4,102 @@ import { v4 } from 'uuid';
 import barrel from '../assets/rum-barrel.jpeg';
 
 
-function KegChange(props) {
-  let _beername = props.beername;
-  let _brand = props.brand;
-  let _price = props.price;
-  let _abv = props.abv;
-  let _img = props.img;
-  let _pintCount = props.pintCount;
+class KegChange extends React.Component {
 
-  function changeKegSubmit() {
-    props.changeKeg({
-      beername:_beername.value,
-      brand:_brand.value,
-      price:_price.value,
-      abv:_abv.value,
-      img:_img.value,
-      pintCount: 124})
+  constructor(props) {
+    super(props);
+    this.state = {
+    beername: props.beername,
+    brand: props.brand,
+    price: props.price,
+    abv: props.abv,
+    img: props.img,
+    pintCount: props.pintCount,
+  };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeKegSubmit = this.handleChangeKegSubmit.bind(this);
+
   }
 
+    handleChange(event) {
+      this.setState({value: event.input});
+    }
 
-  return (
-    <form>
-      <style jsx>{`
+    handleChangeKegSubmit() {
+      props.changeKeg({
+        beername:_beername.value,
+        brand:_brand.value,
+        price:_price.value,
+        abv:_abv.value,
+        img:_img.value,
+        pintCount: 124})
+      }
 
-        button {
-          align-self: center;
-          width: 140px;
-          font-size: .75rem;
-          padding: .25rem 0;
-        }
+    render(props) {
+      return (
+        <form>
+          <style jsx>{`
 
-
-        form {
-          display: grid;
-          grid-template-columns: 1fr 1fr 80px 60px 1fr 150px;
-          grid-auto-flow: row;
-        }
-
-
-
-        img {
-          height: 100%;
-          width: auto;
-          align-self: flex-start;
-        }
-
-        h3 {
-          line-height: 100%;
-          font-size: 1.15rem;
-          margin-bottom: .5rem;
-        }
-
-        input {
-          padding: 0 .25rem;
-        }
-      `}
-      </style>
+              button {
+                align-self: center;
+                width: 140px;
+                font-size: .75rem;
+                padding: .25rem 0;
+              }
 
 
-      <input type = 'text' id = 'beername' name = 'beerName' value = {props.beername}ref = {(input) => {_beername = input}}/>
+              form {
+                display: grid;
+                grid-template-columns: 1fr 1fr 80px 60px 1fr 150px;
+                grid-auto-flow: row;
+              }
 
-      <input type = 'text' id = 'brand' name = 'beerBrand' value = {props.brand} ref = {(input) => {_brand = input}}/>
 
-      <input type = 'number' id = 'price' name = 'beerPrice' value = {props.price} ref = {(input) => {_price = input}}/>
 
-      <input type = 'number' id = 'abv' name = 'beerAbv' value = {props.abv}  ref = {(input) => {_abv = input}}/>
+              img {
+                height: 100%;
+                width: auto;
+                align-self: flex-start;
+              }
 
-      <input type = 'text' id = 'img' name = 'image' value = {props.img} ref = {(input) => {_img = input}}  disabled/>
-      <button onClick={changeKegSubmit}> Save Changes</button>
-    </form>
-  );
+              h3 {
+                line-height: 100%;
+                font-size: 1.15rem;
+                margin-bottom: .5rem;
+              }
+
+              input {
+                padding: 0 .25rem;
+              }
+              `}
+            </style>
+
+
+            <input type = 'text' id = 'beername' name = 'beerName' value = {props.beername}ref = {(input) => {_beername = input}}/>
+
+            <input type = 'text' id = 'brand' name = 'beerBrand' value = {props.brand} ref = {(input) => {_brand = input}}/>
+
+            <input type = 'number' id = 'price' name = 'beerPrice' value = {props.price} ref = {(input) => {_price = input}}/>
+
+            <input type = 'number' id = 'abv' name = 'beerAbv' value = {props.abv}  ref = {(input) => {_abv = input}}/>
+
+            <input type = 'text' id = 'img' name = 'image' value = {props.img} ref = {(input) => {_img = input}}  disabled/>
+            <button onClick={changeKegSubmit}> Save Changes</button>
+          </form>
+        );
+    }
+
 }
 
 KegChange.propTypes = {
-  changeKeg: PropTypes.func,
   img: PropTypes.string,
   beername: PropTypes.string,
   brand: PropTypes.string,
   price: PropTypes.number,
   abv: PropTypes.number,
-  pints: PropTypes.number
+  pints: PropTypes.number,
+  changeKeg: PropTypes.func
 };
+
 
 export default KegChange;
